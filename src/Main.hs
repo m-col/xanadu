6c7c3b45fa71f88a81a76ac4f8aa674bcf13af26
mcol <mcol@posteo.net>
2021-08-19 11:02:08 +0100

    Commit initial somewhat-working version

---
+++ src/Main.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+
+module Main where
+
+import Data.Functor (void)
+import Data.Maybe
+import Data.Void
+import GHC.Int (Int32)
+import System.Directory (getDirectoryContents)
+
+import Data.GI.Base
+import qualified Data.GI.Base.GType as GType
+import qualified GI.Gio as Gio
+import qualified GI.Gtk as Gtk
+
+main :: IO ()
+main = do
+    app <- new Gtk.Application
+      [ #applicationId := "xanadu"
+      , #flags := [ Gio.ApplicationFlagsFlagsNone ]
+      ]
+    on app #activate $ activateApp app
+    Gio.applicationRun app Nothing
+    return ()
+
+activateApp :: Gtk.Application -> IO ()
+activateApp app = do
+    win <- new Gtk.ApplicationWindow
+        [ #application := app
+        , #title := "xanadu"
+        ]
+    listStore <- Gtk.listStoreNew [GType.gtypeString]
+    view <- new Gtk.IconView
+        [ #model := listStore
+        , #textColumn := 0
+        , #pixbufColumn := 1
+        ]
+    populate listStore "/home/mcol/Desktop"
+    #add win view
+    #showAll win
+    return ()
+
+populate :: Gtk.ListStore -> FilePath -> IO ()
+populate listStore root = do
+    contents <- ls root
+    addItems contents
+    return ()
+  where
+    addItems :: [FilePath] -> IO [()]
+    addItems paths = sequence $ map (addItem listStore) paths
+
+addItem :: Gtk.ListStore -> FilePath -> IO ()
+addItem listStore path = do
+    value <- Gtk.toGValue $ Just path
+    iter <- Gtk.listStoreAppend listStore
+    Gtk.listStoreSet listStore iter [0] [value]
+
+ls :: FilePath -> IO [FilePath]
+ls = fmap (filter (\f -> f /= "." && f /= "..")) . getDirectoryContents
