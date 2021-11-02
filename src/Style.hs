37a8d79c3ff8e92f2af04e7218277599cf7c0dfe
mcol <mcol@posteo.net>
2021-11-02 18:16:59 +0000

    Add user custom CSS

--- src/Style.hs
+++ src/Style.hs
@@ -4,17 +4,44 @@ module Style (
     initStyle
 ) where
 
+import Control.Monad (when)
+import System.Directory (doesFileExist, getHomeDirectory)
+import System.Environment (lookupEnv)
+import System.FilePath ((</>))
+
+import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 
 initStyle :: Gtk.ApplicationWindow -> IO ()
 initStyle win = do
+    setTheme win
     addCSS win
+
+setTheme :: Gtk.ApplicationWindow -> IO ()
+setTheme win = do
+    screen <- Gtk.windowGetScreen win
+    settings <- Gtk.settingsGetForScreen screen
+    Gtk.setSettingsGtkApplicationPreferDarkTheme settings True
+
+getConfigDir :: IO FilePath
+getConfigDir = do
+    fromEnv <- lookupEnv "XDG_CONFIG_HOME"
+    case fromEnv of
+        Nothing -> (<> "/.config/xanadu") <$> getHomeDirectory
+        Just "" -> (<> "/.config/xanadu") <$> getHomeDirectory
+        Just path -> return $ path </> "/xanadu"
 
 addCSS :: Gtk.ApplicationWindow -> IO ()
 addCSS win = do
     screen <- Gtk.windowGetScreen win
-    settings <- Gtk.settingsGetForScreen screen
-    Gtk.setSettingsGtkApplicationPreferDarkTheme settings True
     cssProvider <- Gtk.cssProviderNew
-    Gtk.cssProviderLoadFromData cssProvider "* { background-color: transparent; }"
     Gtk.styleContextAddProviderForScreen screen cssProvider 800
+
+    -- Default CSS
+    Gtk.cssProviderLoadFromData cssProvider "* { background-color: transparent; }"
+
+    -- Custom CSS
+    configDir <- getConfigDir
+    let file = configDir </> "style.css"
+    hasFile <- doesFileExist file
+    when hasFile $ Gtk.cssProviderLoadFromFile cssProvider =<< Gio.fileNewForPath file
